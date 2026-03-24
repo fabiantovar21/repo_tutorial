5d2e632b18718e42b4f5306e028e10e6f0ab8cc8
fabiantovar21 <fabian.tovar2002@gmail.com>
2026-03-23 21:53:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ print('Hola mundo!')
 nombre=input('Ingrese su nombre: ')
 
 print(f'hola {nombre}')
+
+numero=int(input('Ingrese un número: '))
+numero2=int(input('Ingrese otro número: '))
+
+print(f'la suma de los numeros es: {numero + numero2}')
